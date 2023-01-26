6c2c90927354d815b9604a7fd92bc0fb3de3e25b
Rokas <rookas.rudzenskas@gmail.com>
2023-01-26 16:53:57 +0200

    feat: add RevenueCat
    - Add RevenueCat to the project
    - Add a hook to use RevenueCat

---
+++ hooks/useRevenueCat.tsx
@@ -0,0 +1,35 @@
+import Purchases, { CustomerInfo, PurchasesOffering } from 'react-native-purchases';
+import {useState} from "react";
+import { useEffect } from 'react';
+import {Platform, StyleSheet} from 'react-native';
+
+// Just random keys, do not work, replace with your own :)
+const APIKeys = {
+  apple: "sk_test_welvlAXoNfPNGpKKeOAIljKJycHcv",
+  google: "sk_test_welvlAXoNfPNGpKKeOAIljKJycHcv"
+}
+
+const typesOfMembership = {
+  monthly: "proMonthly",
+  yearly: "proYearly"
+}
+function useRevenueCat() {
+  const [currentOffering, setCurrentOffering] = useState<PurchasesOffering | null>(null);
+  const [customerInfo, setCustomerInfo] = useState<CustomerInfo | null>(null);
+  const isProMember = customerInfo?.activeSubscriptions.includes(typesOfMembership.yearly) || customerInfo?.activeSubscriptions.includes(typesOfMembership.monthly);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      Purchases.setDebugLogsEnabled(true);
+
+      if(Platform.OS === "android") {
+        await Purchases.configure({ apiKey: APIKeys.google});
+      } else {
+        await Purchases.configure({ apiKey: APIKeys.apple});
+      }
+    }
+    fetchData();
+  }, [])
+}
+
+export default useRevenueCat;
