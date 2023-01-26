7cc6708c56b9a880e601fbc0d722ad55bcbe739f
Rokas <rookas.rudzenskas@gmail.com>
2023-01-26 15:45:23 +0200

    feat: add paywall screen
    - Add paywall screen
    - Add close button

--- screens/PaywallScreen.tsx
+++ screens/PaywallScreen.tsx
@@ -1,14 +1,25 @@
 //@ts-nocheck
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
+import {Ionicons} from "@expo/vector-icons";
+import {useNavigation} from "@react-navigation/native";
 
 const PaywallScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>
-        byrookas 🚀
-      </Text>
-    </View>
+    <ScrollView className="bg-[#1A2F44] flex-1 relative">
+      <View className="m-10 space-y-2">
+        <Text className="text-2xl text-center uppercase text-white font-bold">
+          upgrade
+        </Text>
+        <Text className="text-center text-white">
+          Upgrade to Pro to unlock all features
+        </Text>
+      </View>
+      <TouchableOpacity className="absolute top-5 right-5" onPress={() => navigation.goBack()}>
+        <Ionicons name="md-close-circle-sharp" size={32} color="#e5962D" />
+      </TouchableOpacity>
+    </ScrollView>
   );
 };
 
