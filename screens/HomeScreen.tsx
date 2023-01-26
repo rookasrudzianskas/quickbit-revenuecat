e2e00a7add65271c5dc405c39efd4600adbb0b05
Rokas <rookas.rudzenskas@gmail.com>
2023-01-26 14:32:30 +0200

    feat: add a new feature
    This is a commit message describing the changes and the reasoning behind them.

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -15,20 +15,3 @@ export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>)
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
