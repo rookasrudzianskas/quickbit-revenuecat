2213db8ea5b288a68b7f727502597f98a2dd5cf2
Rokas <rookas.rudzenskas@gmail.com>
2023-01-26 15:03:57 +0200

    feat: add action row
    This commit adds the action row to the home screen.
    
    The action row is a row that will be used to display the user's actions.

---
+++ components/ActionRow.tsx
@@ -0,0 +1,15 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const ActionRow = () => {
+  return (
+    <View>
+      <Text>
+        byrookas 🚀
+      </Text>
+    </View>
+  );
+};
+
+export default ActionRow;
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,13 +3,21 @@ import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import React from "react";
 import {StatusBar} from "expo-status-bar";
-import {Image} from "react-native";
+import {Image, ScrollView, TouchableOpacity} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
 
 export default function HomeScreen({}: RootTabScreenProps<'TabOne'>) {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Image source={{uri: 'https://i.imgur.com/e14NE49.png'}} className="" />
+    <ScrollView>
+      <View className="flex-1 items-center bg-gray-100 pt-10 relative">
+        <TouchableOpacity activeOpacity={0.7} className="absolute z-50 top-12 right-10 items-center">
+          <Ionicons name="person-circle" size={30} color="#e5962D" />
+          <Text className="text-center text-[#e5962D]">UPGRADE</Text>
+        </TouchableOpacity>
+
+        <Image source={{uri: 'https://i.imgur.com/e14NE49.png'}} className="w-full h-64" />
       <StatusBar style="auto" />
-    </View>
+      </View>
+    </ScrollView>
   );
 }
