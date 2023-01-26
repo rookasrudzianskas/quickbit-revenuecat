6ed8f1959aa16e3c04c77cf2cc48e99eb682f71e
Rokas <rookas.rudzenskas@gmail.com>
2023-01-26 14:51:41 +0200

    Add a new feature
    This is a new feature that I've been working on.

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -4,7 +4,7 @@ import { RootTabScreenProps } from '../types';
 import React from "react";
 import {StatusBar} from "expo-status-bar";
 
-export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function HomeScreen({}: RootTabScreenProps<'TabOne'>) {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.js to start working on your app!</Text>
