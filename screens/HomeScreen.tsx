af08927b4b5c5fe60f759cf619395322a6e79893
Rokas <rookas.rudzenskas@gmail.com>
2023-01-26 14:32:12 +0200

    Add a new feature
    This is a new feature that does something.
    
    It is a good feature.

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,34 @@
+// @ts-nocheck
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+import React from "react";
+import {StatusBar} from "expo-status-bar";
+
+export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: '80%',
+  },
+});
