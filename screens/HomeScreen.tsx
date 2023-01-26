28bfe5bee05f566f77b1e23a3f1e4788e555c993
Rokas <rookas.rudzenskas@gmail.com>
2023-01-26 14:55:31 +0200

    Add a cat image to the home screen
    This is a test commit message.

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -3,11 +3,12 @@ import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import React from "react";
 import {StatusBar} from "expo-status-bar";
+import {Image} from "react-native";
 
 export default function HomeScreen({}: RootTabScreenProps<'TabOne'>) {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={{uri: 'https://i.imgur.com/e14NE49.png'}} className="" />
       <StatusBar style="auto" />
     </View>
   );
